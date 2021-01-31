8cfdf3348c409b14c905a7a7503bbe4bbde0220c
francoiducat <francois.ducatillon@decathlon.com>
2021-01-31 11:47:01 +0100

    fix submodule already exists in the index

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+I #!/bin/sh
 
 # If a command fails then the deploy stops
 set -e
@@ -7,19 +7,15 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 # if using a theme, replace with `hugo -t <YOURTHEME>`
-printf "\033[0;32mBuilding the project...\033[0m\n"
 hugo
 
 # Go To Public folder
-printf "\033[0;32mGoing to public...\033[0m\n"
 cd public
 
 # Add changes to git.
-printf "\033[0;32mAdding changes to git...\033[0m\n"
 git add .
 
 # Commit changes.
-printf "\033[0;32mCommiting changes to git...\033[0m\n"
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
